59b4dec8be90a470d9251156ee2a8d2780385740
stevenbachimont <stevenbachimont@gmail.com>
2024-06-14 12:26:45 +0200

    BadgeSeeder, StyleSeeder plus images

---
+++ server/database/fixtures/BadgeSeeder.js
@@ -0,0 +1,41 @@
+const AbstractSeeder = require('./AbstractSeeder');
+
+
+class BadgeSeeder extends AbstractSeeder {
+    constructor() {
+        super({ table: 'badge', truncate: true });
+    }
+
+    run() {
+        const art = [
+            {
+                name: 'Gold',
+                img: 'https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/golden.png',
+                scenario: 'vous avez trouvé 100 graffiti',
+                level: '3'
+            },
+
+            {
+                name: 'Silver',
+                img: 'https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/golden.png',
+                scenario: 'vous avez trouvé 50 graffiti',
+                level: '2'
+            },
+
+            {
+                name: 'Bronze',
+                img: 'https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/golden.png',
+                scenario: 'vous avez trouvé 10 graffiti',
+                level: '1'
+            }
+        ];
+
+
+        // eslint-disable-next-line no-shadow
+        art.forEach((art) => {
+            this.insert(art);
+        });
+
+    }
+}
+module.exports = BadgeSeeder;
