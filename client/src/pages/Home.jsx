7e3b395c71fd0807f60625067aadfe23caab275e
stevenbachimont <stevenbachimont@gmail.com>
2024-06-18 18:42:47 +0200

    maj component photo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/extensions
-import { Photo2 } from "../components/Photo2.jsx";
+import { Photo } from "../components/Photo.jsx";
 
 function Home() {
   return (
     <main>
       <h1>WELCOME HOME</h1>
-      <Photo2 />
+      <Photo />
     </main>
   );
 }
