8338a5eb544e49a35a9abd6375eed9ab280189dd
brewost <156411525+brewost@users.noreply.github.com>
2024-06-19 12:34:51 +0200

    added map page to pages and moved the GraffitiMap component to it.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,11 @@
-
 // eslint-disable-next-line import/extensions
 import { Photo } from "../components/Photo.jsx";
-
-import GraffitiMap from "../components/GraffitiMap";
-
-
 
 function Home() {
   return (
     <main>
       <h1>WELCOME HOME</h1>
-
-
-
-
-
-      {/* <div id="map" style={{ height: "180px" }} /> */}
-
-      <GraffitiMap />
       <Photo />
-
     </main>
   );
 }
