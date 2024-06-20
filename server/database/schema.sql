4b59d3b671d78358667e3176b3f9c83e9dd47d2c
brewost <156411525+brewost@users.noreply.github.com>
2024-06-20 16:41:04 +0200

    change sql database for uppercase letters and indentation

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,67 +1,67 @@
 CREATE TABLE `user` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `alias` varchar(30),
-    `email` varchar(30),
-    `password` varchar(255),
-    `profile_picture` varchar(255),
-    `is_admin` bool,
-    `is_verify` bool,
-    `graffiti_geek_level` integer
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `alias` VARCHAR(30),
+    `email` VARCHAR(30),
+    `password` VARCHAR(255),
+    `profile_picture` VARCHAR(255),
+    `is_admin` BOOL,
+    `is_verify` BOOL,
+    `graffiti_geek_level` INT
 );
 
 CREATE TABLE `hood` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `hood_name` varchar(20),
-    `city` bool,
-    `suburbs` bool
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `hood_name` VARCHAR(20),
+    `city` BOOL,
+    `suburbs` BOOL
 );
 
 CREATE TABLE `style` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `name` varchar(20) NOT NULL,
-    `style_tag` varchar(150) NOT NULL
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `style_tag` VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE `art` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `user_id` int,
-    `is_verify` bool,
-    `img_date` date NOT NULL,
-    `artist` varchar(20),
-    `style` varchar(20),
-    `image` varchar(255),
-    `image_alt` varchar(255),
-    `gps_lat` int NOT NULL,
-    `gps_long` int NOT NULL,
-    `hood_id` int,
-    `size` varchar(20),
-    `still_up` bool,
-    `verifier_by` varchar(20),
-    `graffiti_date` date,
-    `zone` int,
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT,
+    `is_verify` BOOL,
+    `img_date` DATE NOT NULL,
+    `artist` VARCHAR(20),
+    `style` VARCHAR(20),
+    `image` VARCHAR(255),
+    `image_alt` VARCHAR(255),
+    `gps_lat` INT NOT NULL,
+    `gps_long` INT NOT NULL,
+    `hood_id` INT,
+    `size` VARCHAR(20),
+    `still_up` BOOL,
+    `verifier_by` VARCHAR(20),
+    `graffiti_date` DATE,
+    `zone` INT,
     FOREIGN KEY (`hood_id`) REFERENCES `hood` (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE `badge` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `name` varchar(20) NOT NULL,
-    `img` varchar(255) NOT NULL,
-    `scenario` varchar(100),
-    `level` int
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `img` VARCHAR(255) NOT NULL,
+    `scenario` VARCHAR(100),
+    `level` INT
 );
 
 CREATE TABLE `user_badge` (
-    `user_id` int,
-    `badge_id` int,
+    `user_id` INT,
+    `badge_id` INT,
     FOREIGN KEY (`badge_id`) REFERENCES `badge` (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
     PRIMARY KEY (`user_id`, `badge_id`)
 );
 
 CREATE TABLE `art_style` (
-    `art_id` int,
-    `style_id` int,
+    `art_id` INT,
+    `style_id` INT,
     FOREIGN KEY (`style_id`) REFERENCES `style` (`id`),
     FOREIGN KEY (`art_id`) REFERENCES `art` (`id`),
     PRIMARY KEY (`art_id`, `style_id`)
