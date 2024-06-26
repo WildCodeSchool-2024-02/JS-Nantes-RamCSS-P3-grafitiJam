352a2a94bf515f02587a490ad8299152e476b428
stevenbachimont <stevenbachimont@gmail.com>
2024-06-26 16:52:54 +0200

    remplissage termpage

--- client/src/components/lexiques/TermsBadge.jsx
+++ client/src/components/lexiques/TermsBadge.jsx
@@ -6,56 +6,69 @@ function TermsBadge() {
       title: "Gold",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/golden.png",
-      explanation: "Explication pour Gold",
+      explanation:
+        "Une fois que vous avez scanné et enregistré 50 graffitis différents, validés par les experts, vous recevrez le badge Golden. Vous pourrez voir ce badge dans votre profil et vous deviendrez validateur expert.",
     },
     {
       title: "Silver",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/silver.png",
-      explanation: "Explication pour Silver",
+      explanation:
+        "Une fois que vous avez scanné et enregistré 25 graffitis différents, validés par les experts, vous recevrez le badge Silver. Ce badge sera visible dans votre profil.",
     },
     {
       title: "Bronze",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/bronze.png",
-      explanation: "Explication pour Bronze",
+      explanation:
+        "Une fois que vous avez scanné et enregistré 10 graffitis différents, validés par les experts, vous recevrez le badge Bronze. Vous pourrez voir ce badge dans votre profil.",
     },
     {
       title: "Level 1",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/1.png",
-      explanation: "Explication pour Level 1",
+      explanation:
+        "Description : Ce badge est obtenu lorsque vous trouvez et scannez 5 graffitis dans un même style.\n" +
+        "Objectif : Recherchez et enregistrez 5 graffitis qui partagent des caractéristiques stylistiques similaires.",
     },
     {
       title: "Level 2",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/2.png",
-      explanation: "Explication pour Level 2",
+      explanation:
+        "Description : Ce badge est obtenu lorsque vous trouvez et scannez 10 graffitis dans un même style.\n" +
+        "Objectif : Recherchez et enregistrez 10 graffitis avec des éléments de style communs.",
     },
     {
       title: "Level 3",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/3.png",
-      explanation: "Explication pour Level 3",
+      explanation:
+        "Description : Ce badge est obtenu lorsque vous trouvez et scannez 20 graffitis dans un même style.\n" +
+        "Objectif : Recherchez et enregistrez 20 graffitis ayant des caractéristiques stylistiques similaires.",
     },
     {
       title: "Level 4",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/badges/4.png",
-      explanation: "Explication pour Level 4",
+      explanation:
+        "Description : Ce badge est obtenu lorsque vous trouvez et scannez 50 graffitis dans un même style.\n" +
+        "Objectif : Recherchez et enregistrez 50 graffitis qui appartiennent à un même style artistique.",
     },
   ];
 
   return (
     <>
       {badges.map((badge, index) => (
-          // eslint-disable-next-line react/no-array-index-key
+        // eslint-disable-next-line react/no-array-index-key
         <div key={index} className="terms-badge-container">
           <div className="title-image">
             <h2>{badge.title}</h2>
             <img src={badge.image} alt={badge.explanation} />
           </div>
-          <p>{badge.explanation}</p>
+          <div className="scrollable-div">
+            <p>{badge.explanation}</p>
+          </div>
         </div>
       ))}
     </>
--- client/src/components/lexiques/TermsTag.jsx
+++ client/src/components/lexiques/TermsTag.jsx
@@ -3,63 +3,91 @@ import "./styles/Tag.css";
 function TermsTag() {
   const tags = [
     {
-      title: "Buble",
+      title: "Flop",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/buble.svg",
-      explanation: "Explication pour Buble",
+      alt: "Flop",
+      explanation:
+        "Le flop s’inscrit dans la continuité du tag : plus grand et plus lisible, c’est son prolongement direct. Destiné à la partie vandale, il présente l’avantage d’être réalisé rapidement. En effet, le throw-up consiste à réaliser de manière fluide et rapide un lettrage aux formes simples, plus ou moins arrondies, qui doit être lisible au premier regard. Il est généralement constitué de deux couleurs, la plus foncée servant à réaliser le contour des lettres.\n" +
+        "\n" +
+        "De même que pour le tag, la pratique et l’entraînement peuvent être longs avant de maitriser cette technique. Ce qui peut sembler simple de prime abord se révèle complexe dans la réalisation pour obtenir un flop de qualité. De nombreux writers sont reconnus pour la qualité (et la quantité) de leur throw-up : Seen, JonOne, Cope2, Utah et Ether, Reso, Tilt, Ahero, Horfe, pour ne citer qu’eux !",
     },
     {
-      title: "Cubic",
+      title: "Block Letter",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/cubic.svg",
-      explanation: "Explication pour Cubic",
+      alt: "Block Letter",
+      explanation:
+        "Le block letter est composé d’énormes lettres, de plusieurs mètres de haut. Généralement réalisé le long des voies ferrées, les bords d’autoroute ou des bâtiments désaffectés, car il est plus long à peindre. Certains vont toutefois réaliser des block letters sur des wagons entiers (whole car), voire des trains entiers (whole train).\n" +
+        "\n" +
+        "De formes simples pour être lisibles de loin, les lettres sont généralement réalisées en deux couleurs, à la peinture acrylique à l’aide de rouleaux ou intégralement à la spray. L’objectif du block letter : faire plus gros !",
     },
     {
-      title: "Figure",
+      title: "Cartoon",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/figure.svg",
-      explanation: "Explication pour Figure",
+      alt: "Cartoon",
+      explanation:
+        "Le cartoon est un dessin destiné aux films d’animation ou bandes dessinées. Les lignes et les formes réalisées pour ce genre de réalisations sont simples. L’âge d’or du cartoon se situe entre les années 30 et les années 50 avec les productions de Walt Disney ou de Warner Bros. Un graffiti au style cartoon se caractérise par des lignes simples aux couleurs vives et aux représentations rigolotes. On retrouve dans ce style des œuvres figuratives de végétaux ou animaux personnifiées ou des personnages de la culture cinématographique. \n" +
+        "\n" +
+        "Le style cartoon demande du temps de réalisation, car bien que les traits soient simples, le traitement des couleurs peut demander du temps. ",
     },
     {
-      title: "Lettring",
+      title: "Wildstyle",
       image:
-        "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/lettring.svg",
-      explanation: "Explication pour Lettring",
+        "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/wild.svg",
+      alt: "Wildstyle",
+      explanation:
+        "Le wild style porte bien son nom : style sauvage. Le travail des lettres est poussé à l’extrême, à tel point que l’œuvre est quasiment illisible pour un œil non habitué. Les lettres sont déformées, enchevêtrées, enrichies d’ajouts graphiques (flèches, étoiles, vagues…).\n" +
+        "\n" +
+        "L’ajout d’une “épaisseur“ donnant l’impression d’un relief (3D) apporte de la personnalité et améliore la lisibilité aux lettres.\n" +
+        "\n" +
+        "Le travail des couleurs joue également un rôle important dans ce type de graffiti, qui devient une forme globale, graphiquement esthétique.",
     },
     {
-      title: "S",
+      title: "Tag",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/tag.svg",
-      explanation: "Explication pour Tag",
+      alt: "Tag",
+      explanation:
+        "Le tag est à la base de tous les autres styles. On le définit comme la signature du graffeur. Celui-ci peut être réalisé rapidement et permet d’écrire un grand nombre de fois son pseudonyme. Il est également apposé pour signer un lettrage ou une fresque. Généralement, le taggeur travaille son style au fil des ans, et sa signature devient identifiable au premier coup d’œil pour qui y est attentif.\n" +
+        "C’est un vrai travail de calligraphie : les lettres de l’alphabet deviennent un terrain d’entraînement pour arriver à une signature unique.\n" +
+        "\n" +
+        "Pour beaucoup de graffeurs, le tag reste un critère d’évaluation du niveau technique et de la dextérité d’un peintre : une belle fresque signée avec un tag mal exécuté trahit un manque de pratique dans ce domaine.",
     },
     {
-      title: "M",
+      title: "Small",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/S.svg",
-      explanation: "Explication pour S",
+      alt: "Small",
+      explanation: "Explication pour Ssmall",
     },
     {
-      title: "L",
+      title: "Medium",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/M.svg",
-      explanation: "Explication pour M",
+      alt: "Medium",
+      explanation: "Explication pour Medium",
     },
     {
-      title: "XL",
+      title: "Large",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/L.svg",
-      explanation: "Explication pour L",
+      alt: "Large",
+      explanation: "Explication pour Large",
     },
     {
-      title: "Monumental",
+      title: "XtraLarge",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/XL.svg",
-      explanation: "Explication pour XL",
+      alt: "XtraL",
+      explanation: "Explication pour XtraLarge",
     },
     {
       title: "Monumental",
       image:
         "https://raw.githubusercontent.com/WildCodeSchool-2024-02/JS-Nantes-RamCSS-P3-grafitiJam/main/server/public/assets/tags/Mtl.svg",
+      alt: "Monumental",
       explanation: "Explication pour Monumental",
     },
   ];
@@ -68,13 +96,15 @@ function TermsTag() {
     <>
       {/* eslint-disable-next-line no-shadow */}
       {tags.map((tags, index) => (
-          // eslint-disable-next-line react/no-array-index-key
+        // eslint-disable-next-line react/no-array-index-key
         <div key={index} className="terms-badge-container">
           <div className="title-image">
             <h2>{tags.title}</h2>
-            <img src={tags.image} alt={tags.explanation} />
+            <img src={tags.image} alt={tags.alt} />
           </div>
-          <p>{tags.explanation}</p>
+          <div className="scrollable-div">
+            <p>{tags.explanation}</p>
+          </div>
         </div>
       ))}
     </>
