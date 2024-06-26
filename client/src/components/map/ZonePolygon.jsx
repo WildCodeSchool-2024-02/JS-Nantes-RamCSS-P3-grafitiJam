fe797c00284c27f4ec250bced6c09bd7886fc3e3
stevenbachimont <stevenbachimont@gmail.com>
2024-06-26 20:36:14 +0200

    délimitation de Reze et ajout de user localisation sur map

---
+++ client/src/components/map/ZonePolygon.jsx
@@ -0,0 +1,24 @@
+import { Polygon, Popup } from "react-leaflet";
+
+// eslint-disable-next-line react/prop-types
+function ZonePolygon({ positions }) {
+    const handleClick = () => {
+        // eslint-disable-next-line no-alert
+        alert("Vous avez cliqué sur le polygone");
+
+    };
+
+    return (
+        <Polygon
+            positions={positions}
+            color="blue"
+            fillColor="blue"
+            fillOpacity={0.1}
+            onClick={handleClick}
+        >
+            <Popup>Ici c'est Rezé</Popup>
+        </Polygon>
+    );
+}
+
+export default ZonePolygon;
