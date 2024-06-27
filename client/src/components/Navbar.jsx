faeb97766e6d726ed8757abd848f017378720bec
Audren <audren.destin@gmail.com>
2024-06-27 16:03:35 +0200

    navbar en cours

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,22 @@
 import { Link } from "react-router-dom";
+import "../pages/styles/navbar.css"
+/* import GallerieIcon from "../../../server/public/assets/icones/Gallerie.svg";
+import MapIcon from "../../../server/public/assets/icones/Map.svg";
+import PhotoIcon from "../../../server/public/assets/icones/Photo.svg";
+import ToHomeIcon from "../../../server/public/assets/icones/toHome.svg"; */
 
 function Navbar() {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/auth">Auth</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/terms">Terms</Link>
-        </li>
-        <li>
-          <Link to="/map">Map</Link>
-        </li>
-        <li>
-          <Link to="/photo">Photo</Link>
-        </li>
-        <li>
-          <Link to="/galerie">Galerie</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/auth">Auth</Link></li>
+        {/* <li><Link to="/profile">Profile</Link></li> */}
+        <li><Link to="/terms">Terms</Link></li>
+ 
+        {/* <li><Link to="/map"><img src={MapIcon} alt="Map" /></Link></li> */}
+        {/* <li><Link to="/photo"><img src={PhotoIcon} alt="Photo" /></Link></li> */}
+        {/* <li><Link to="/galerie"><img src={GallerieIcon} alt="Gallerie" /></Link></li> */}
       </ul>
     </nav>
   );
