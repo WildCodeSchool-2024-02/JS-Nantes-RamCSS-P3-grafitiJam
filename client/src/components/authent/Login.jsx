3d2747a1913b2be0ac6d2b196fbd36b9b8c95731
brewost <156411525+brewost@users.noreply.github.com>
2024-06-28 12:29:41 +0200

    update login form

--- client/src/components/authent/Login.jsx
+++ client/src/components/authent/Login.jsx
@@ -2,8 +2,51 @@ import "./styles/Login.css";
 
 // eslint-disable-next-line react/prop-types
 function Login({ showRegister }) {
-  const handleLogin = () => {
-    // Add login logic here
+  // eslint-disable-next-line no-undef
+  const [isEmail, setIsEmail] = useState(true);
+  // eslint-disable-next-line no-undef
+  const [isPassword, setIsPassword] = useState(true);
+
+  const handleFetch = async (data) => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      setIsEmail(() => false);
+      setIsPassword(() => false);
+    } else {
+      const res = await response.json();
+      localStorage.setItem("token", res.token);
+      console.info("Logged", res);
+    }
+  };
+
+  const handleLogin = async (event) => {
+    try {
+      event.preventDefault();
+
+      const email = event.target.email.value;
+      // eslint-disable-next-line no-undef
+      const isEmailValid = emailValidation(email);
+
+      const password = event.target.password.value;
+      // eslint-disable-next-line no-undef
+      const isPasswordValid = passwordValidation(password);
+
+      setIsEmail(() => isEmailValid);
+      setIsPassword(() => isPasswordValid);
+
+      if (isEmailValid && isPasswordValid) {
+        await handleFetch({ email, password });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   const handleSignUp = () => {
@@ -21,6 +64,8 @@ function Login({ showRegister }) {
           name="email"
           placeholder="Email"
           required
+          autoComplete="off"
+          onFocus={() => !isEmail && setIsEmail(true)}
         />
       </div>
       <div className="form-group">
@@ -31,10 +76,12 @@ function Login({ showRegister }) {
           name="password"
           placeholder="password"
           required
+          autoComplete="off"
+          onFocus={() => !isPassword && setIsPassword(true)}
         />
       </div>
       <div className="form-buttons">
-        <button type="button" id="loginButton" onClick={handleLogin}>
+        <button type="submit" id="loginButton" onSubmit={handleLogin}>
           Login
         </button>
         <button type="button" id="signUpButton" onClick={handleSignUp}>
