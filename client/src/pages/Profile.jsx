2f5a7ff118c15409fa30539069dec77c84fcbedb
Audren <audren.destin@gmail.com>
2024-07-09 14:46:14 +0200

    récupération des données dans la page Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,13 @@
 
-import './styles/profile.css'
+import './styles/profile.css';
+import { useContext } from 'react';
+import { ConnexionContext } from '../Contextes/ConnexionContexte';
+
 
 function Profile() {
+  const { isConnected, alias, graffitiGeekLevel, userId } = useContext(ConnexionContext);
+
+
   return (
     <main>
       <div className='profile-container'>
@@ -12,16 +18,18 @@ function Profile() {
       <div className='infos-profile-container'>
       
       <div className='alias-container'>
-        <p className='alias-text'>Alias : Zorro</p>
+        {isConnected && (
+          <p className='alias-text'>Alias : {alias}</p>
+        )}
+
       </div>
 
 <div className='geek-level-container'>
   <p className='geek-level-text'>Geek level</p>
   <div className='level-indicators'>
-    <div className='level-indicator' />
-    <div className='level-indicator' />
-    <div className='level-indicator' />
-    <div className='level-indicator' />
+  {Array.from({ length: graffitiGeekLevel }).map(() => (
+  <div key={`level-${userId}`} className='level-indicator' />
+))}
   </div>
 </div>
 
